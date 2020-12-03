23506f758852ed054e434a00f5b442d3f253d4e2
Asif623 <mkhan135@fiu.edu>
2020-12-03 15:50:16 -0500

    build added

---
+++ precache-manifest.b91b6ccdc64d192df6b979cc909c63cc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b1f4af25008cdcfb6eccf1a8edf747b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b62b2940955d159a140a",
+    "url": "/static/css/2.e6f51e71.chunk.css"
+  },
+  {
+    "revision": "5b10f4425eb7fb4d920f",
+    "url": "/static/css/main.f5f8e4bb.chunk.css"
+  },
+  {
+    "revision": "b62b2940955d159a140a",
+    "url": "/static/js/2.7cc04d31.chunk.js"
+  },
+  {
+    "revision": "c29bf2bc297e5b651e9fc4fba4b356f1",
+    "url": "/static/js/2.7cc04d31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b10f4425eb7fb4d920f",
+    "url": "/static/js/main.8d57a10a.chunk.js"
+  },
+  {
+    "revision": "6c850cfe1a4fab9c490f",
+    "url": "/static/js/runtime-main.be5ffbac.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
